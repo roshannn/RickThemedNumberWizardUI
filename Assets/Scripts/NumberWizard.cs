db83ba91926a9bdd92a74685f89f120acd31e237
roshan98 <roshansingh1912@gmail.com>
2021-03-24 20:28:11 +0530

    Exported too many resources. The game works fine export to play

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int min;
+    [SerializeField] int max;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+    void Start()
+    {
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess + 1;
+        NextGuess();
+    }
+    public void OnPressLower()
+        {
+            max = guess - 1;
+        NextGuess();
+    }
+    void NextGuess()
+    {
+        guess = (min + max) / 2;
+        guessText.text = guess.ToString();  
+    }
+}
+
